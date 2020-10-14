be5db10e540cf9fccf3f62c2e14ffd9fa6e97a34
lina mierkE <lina_maria89@hotmail.com>
2020-10-13 21:49:46 -0400

    phase1

---
+++ script.js
@@ -0,0 +1,14 @@
+//container
+//cool title
+//cool background
+//question(s)
+//options answer(s)/buttons
+
+//1. when the page load the message and button of start is shown 
+
+//user will click in the botton which will take them to the first question 
+//2 the first question is shown with 4 options 1 is correct 4 are incorrect
+//3. Second question with picture with different options is load 
+//4. score is load .. maybe 1/5 or maybe % 
+// 5 a botton to try again is loaded
+console.log("hello")
