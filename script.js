5b7acdff91c091b66a46d8234ccdec9459b7fb40
lina mierkE <lina_maria89@hotmail.com>
2020-10-14 16:36:51 -0400

    grids, css, click event

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-//container
-//cool title
-//cool background
-//question(s)
-//options answer(s)/buttons
 
-//1. when the page load the message and button of start is shown 
-
-//user will click in the botton which will take them to the first question 
-//2 the first question is shown with 4 options 1 is correct 4 are incorrect
-//3. Second question with picture with different options is load 
-//4. score is load .. maybe 1/5 or maybe % 
-// 5 a botton to try again is loaded
-// let questions = document.createElement('div');
-// questions.className= "questions";
-// questions.innerText= "First Question";
-// document.body.appendChild(questions);
-// console.log(questions);
 
 let button= document.querySelector(".start");
 button.addEventListener('click', function(e){
-    let questions= document.querySelector(".questions")
-    if(questions.style.display == 'block'){
-        e.style.display== 'none'
-  return questions;
-    }
-    })
+    button.style.display = "none";
+    let trivia = document.querySelector("h2");
+    console.log(trivia)
+    trivia.style.display = "none";
+    let q1= document.querySelector(".q1")
+    q1.style.display= "block";
+})
+
+let q1right = document.querySelector('.right')
+q1right.addEventListener('click', function(e){
+     q1right.style.display= 'none'
+    let answer1= document.querySelector('.q1')
+    answer1.style.display = 'none'
+    let q2= document.querySelector('.q2')
+    q2.style.display = "block";
+})
+   
+
+
+
+
+
 
